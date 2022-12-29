abab1418b33fd2f672453461d504dd8e442e28e8
latoadeoye <latoadeoye@yahoo.com>
2022-12-29 04:09:51 -0800

    Work in progress

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-sequences */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-undef */
 // eslint-disable-next-line no-undef
 // eslint-disable-next-line no-lone-blocks
 // eslint-disable-next-line no-unused-expressions
 // eslint-disable-next-line no-lone-blocks
-butto=buttonimport; { render, screen } 'from={_FormDataEvent:@testing-library/react}';
-import App from './App';
+'varbutton';'buttonimport'; { 'render', screen } 'from={_FormDataEvent:@testing-library/react}';
 
 test('renders learn react link', () => {
-
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+               'render(App /)';   'expect(linkElement).toBeInTheDocument()'
 });
--- src/index.js
+++ src/index.js
@@ -5,39 +5,41 @@
 import React, {} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
- const root = ReactDOM.createRoot(document.getElementById('root'))({
-     children(_ANYComponent){},
-       React(typeOfReactMode,anyAsyncIterable){
-   func(
-     root.render(),
-     React.ReactNode,<react></react>),
-     anyelement='truetoggleAttribute(attrName)'
-    implement='implementPropsanyimplement'
-    this.props.Appimplemrnt,'CloudFormationanyStack';anyAsyncIterable,
-    <legend>implementPropsanyimplement</legend>,'implementPropsanyimplement',this.props.Appimplemrnt,'CloudFormationanyStack';anyAsyncIterable,
-    DOMImplementation,
-    varDOMImplementation;{}
-    newDOMImplementation,
-  <React.StrictMode/>,
-  this
-    this.children('ANYComponent','any');
-    this.func();
-  {}
-      this.props.App();implementation(newEPPName='CLOUDFORMATIOM'),
-    ReactDOMdocument.getElementById('id');
-  {}
+let root;
+
+root = ReactDOM.createRoot(document.getElementById('root'))({
+    children(_ANYComponent){},
+      React(typeOfReactMode,anyAsyncIterable){
+  func(
+    root.render(),
+    React.ReactNode,<react></react>),
+    anyelement='truetoggleAttribute(attrName)'
+   implement='implementPropsanyimplement'
+   this.props.Appimplemrnt,'CloudFormationanyStack';anyAsyncIterable,
+   <legend>implementPropsanyimplement</legend>,'implementPropsanyimplement',this.props.Appimplemrnt,'CloudFormationanyStack';anyAsyncIterable,
+   DOMImplementation,
+   varDOMImplementation;{}
+   newDOMImplementation,
+ <React.StrictMode/>,
+ this
+   this.children('ANYComponent','any');
+   this.func();
+ {}
+     this.props.App();implementation(newEPPName='CLOUDFORMATIOM'),
+   ReactDOMdocument.getElementById('id');
+ {}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 report(reportWebVitals(('On','PerEntries'),
-  console.log('varDataTransfer'),
-  Console.log('newDataTransfer'),
-  Console.log('dataany'),
-  Console.log('anytime'),
-  Console.log('varlocation'),
-  Console.log('anyStatus'),
-  console.log('functionReact'),
-  console.log('ReactDOMdocument.getElementById(id)'),
-  conssole.log('ReadableStreamDefaultController'),       
-    render('children','ReactNode'),
-  this.propselement.setAttributeNode(('attributeNode'),<any>any</any>,'isSecureConstantSourceCode','varCountQueuingStrategy'),))},Props:'anyAsyncIterable',})                                             
+ console.log('varDataTransfer'),
+ Console.log('newDataTransfer'),
+ Console.log('dataany'),
+ Console.log('anytime'),
+ Console.log('varlocation'),
+ Console.log('anyStatus'),
+ console.log('functionReact'),
+ console.log('ReactDOMdocument.getElementById(id)'),
+ conssole.log('ReadableStreamDefaultController'),       
+   render('children','ReactNode'),
+ this.propselement.setAttributeNode(('attributeNode'),<any>any</any>,'isSecureConstantSourceCode','varCountQueuingStrategy'),))},Props:'anyAsyncIterable',});                                             
